0864c4894d364ffd532db0b74c6db2b6ac2aae1c
Iliya Lobkov <iliyaandaleks@mail.ru>
2020-02-04 23:53:51 +0300

    перенос

---
+++ Project_SkillBox_Black_Star/Pods/Target Support Files/Pods-Project_SkillBox_Black_Star/Pods-Project_SkillBox_Black_Star-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Project_SkillBox_Black_Star : NSObject
+@end
+@implementation PodsDummy_Pods_Project_SkillBox_Black_Star
+@end
---
+++ Project_SkillBox_Black_Star/Project_SkillBox_Black_Star/Json/JSONRequest.swift
@@ -0,0 +1,18 @@
+//
+//  JSONRequest.swift
+//  Project_SkillBox_Black_Star
+//
+//  Created by Илья Лобков on 04/02/2020.
+//  Copyright © 2020 Илья Лобков. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class JsonRequest {
+    
+    func loadCategory(callback: @escaping ([Category])->()){
+        Alamofire.request(<#T##url: URLConvertible##URLConvertible#>)
+    }
+    
+}
