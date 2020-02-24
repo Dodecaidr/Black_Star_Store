4869595a7df272072af8ca992cc0a102c2027eca
Iliya Lobkov <iliyaandaleks@mail.ru>
2020-02-24 17:22:16 +0300

    Image
    сделаны нормальные размеры картинок и ползунков

--- Project_SkillBox_Black_Star/Project_SkillBox_Black_Star/Product/Product_Description/ProductDescriptionViewController.swift
+++ Project_SkillBox_Black_Star/Project_SkillBox_Black_Star/Product/Product_Description/ProductDescriptionViewController.swift
@@ -23,36 +23,45 @@ class ProductDescriptionViewController: UIViewController, UIScrollViewDelegate {
     
     var products: ItemProduct?
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+         imageScrolView.frame = CGRect(x: 0, y: 0, width:UIScreen.main.bounds.width , height:imageFrameView.frame.height)
+        var countImage = 0
+              imagePageControl.numberOfPages = products?.productImages.count ?? 0
+              countImage = products?.productImages.count ?? 0
+        
+        for i in 0..<countImage {
+         
+         let imageView = UIImageView()
+         imageView.contentMode = .scaleToFill
+         imageView.sd_setImage(with: URL(string:"https://blackstarwear.ru/\(String(products?.productImages[i].imageURL ?? "sad"))"), completed: nil)
+        
+         
+         let xPos = CGFloat(i) * self.view.bounds.size.width
+         imageView.frame = CGRect(x: xPos, y: 0, width: view.frame.size.width, height: imageFrameView.frame.size.height)
+         imageScrolView.contentSize.width = view.frame.size.width * CGFloat(i+1)
+         imageScrolView.addSubview(imageView)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.overrideUserInterfaceStyle = .light
+        
         othersTabelView.isHidden = true
-        var countImage = 0
-        imagePageControl.numberOfPages = products?.productImages.count ?? 0
-        countImage = products?.productImages.count ?? 0
+      
         
         var price = ""
         let priceSepar = products?.price?.split(separator: ".")
         price = price + (priceSepar?[0] ?? "цена не указана")
         
-       imageScrolView.frame = CGRect(x: 0, y: 0, width:UIScreen.main.bounds.width , height:imageFrameView.frame.height)
-        
+      
         nameProductLabel.text = products?.englishName
         priceLabel.text = price + " ₽"
         descriptionTextView.text = products?.description?.htmlToString
         
-        for i in 0..<countImage {
-            
-            let imageView = UIImageView()
-            imageView.contentMode = .scaleToFill
-            imageView.sd_setImage(with: URL(string:"https://blackstarwear.ru/\(String(products?.productImages[i].imageURL ?? "sad"))"), completed: nil)
-           
-            
-            let xPos = CGFloat(i) * self.view.bounds.size.width
-            imageView.frame = CGRect(x: xPos, y: 0, width: view.frame.size.width, height: imageFrameView.frame.size.height)
-            imageScrolView.contentSize.width = view.frame.size.width * CGFloat(i+1)
-            imageScrolView.addSubview(imageView)
-        }
         
     }
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
